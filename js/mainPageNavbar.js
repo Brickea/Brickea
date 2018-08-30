11fa1df00d4d5b312d87f04d1d336f98033b9872
Brickea <Brickea@foxmail.com>
2018-08-30 21:08:05 +0800

    js目录组件初始化

---
+++ js/mainPageNavbar.js
@@ -0,0 +1,129 @@
+// @Author:Brickea
+// @Date:2018年8月30日
+// @Function:主页面目录组件
+// @Description:gitpage目前根目录是https://brickea.github.io/Brickea/
+
+$(document).ready(function () {
+    var mainPath = "https://brickea.github.io/Brickea/";
+    var mainList = document.getElementById("menuList"); //获取页面上目录
+
+    var liHomePage = document.createElement("li");
+    var aHomePage = document.createElement("a");
+
+    aHomePage.setAttribute("href",mainPath + "index.html");
+    liHomePage.setAttribute("class","active")
+    aHomePage.innerText = "首页";
+
+    liHomePage.appendChild(aHomePage);
+
+    mainList.appendChild(liHomePage);
+    // ---首页创建完毕
+
+    var liNotePage = document.createElement("li");
+
+    var aNotePage = document.createElement("a");
+    aNotePage.setAttribute("href","#");
+    aNotePage.setAttribute("class","dropdown-toggle active")
+    aNotePage.setAttribute( "data-toggle","dropdown")
+    aNotePage.setAttribute("role","button")
+    aNotePage.setAttribute("aria-haspopup","true")
+    aNotePage.setAttribute("aria-expanded", "false")
+    aNotePage.innerText = "笔记"
+
+    var spanNotePage = document.createElement("span");
+    spanNotePage.setAttribute("class", "caret");
+
+    aNotePage.appendChild(spanNotePage);
+
+    var ulNotePage = document.createElement("ul");
+    ulNotePage.setAttribute("class", "dropdown-menu");
+
+    var liNoteFCC = document.createElement("li");
+    var aNoteFCC = document.createElement("a");
+    aNoteFCC.setAttribute("href", mainPath + "notes/FCC_notes/note_0.html");
+    aNoteFCC.innerText = "FreeCodeCamp学习笔记";
+    liNoteFCC.appendChild(aNoteFCC);
+
+    var liNoteJS = document.createElement("li");
+    var aNoteJS = document.createElement("a");
+    aNoteJS.setAttribute("href", mainPath + "notes/FCC_notes/note_0.html");
+    aNoteJS.innerText = "JS奇淫技巧";
+    liNoteJS.setAttribute("class","disabled");
+    liNoteJS.appendChild(aNoteJS);
+
+    var liNoteIn = document.createElement("li");
+    var aNoteIn = document.createElement("a");
+    aNoteIn.setAttribute("href", mainPath + "notes/Interviews/interview_0.html");
+    aNoteIn.innerText = "面试笔记";
+    liNoteIn.appendChild(aNoteIn);
+
+    ulNotePage.appendChild(liNoteFCC);
+    ulNotePage.appendChild(liNoteJS);
+    ulNotePage.appendChild(liNoteIn);
+    
+    liNotePage.appendChild(aNotePage);
+    liNotePage.appendChild(ulNotePage);
+
+    mainList.appendChild(liNotePage);
+    // ---笔记创建完毕
+
+    var liGitPage = document.createElement("li");
+    var aGitPage = document.createElement("a");
+
+    aGitPage.setAttribute("href","https://github.com/Brickea");
+    aGitPage.innerText = "My Github";
+
+    liGitPage.appendChild(aGitPage);
+
+    mainList.appendChild(liGitPage);
+    // ---My Git创建完毕
+
+    var liStoryPage = document.createElement("li");
+
+    var aStoryPage = document.createElement("a");
+    aStoryPage.setAttribute("href","#");
+    aStoryPage.setAttribute("class","dropdown-toggle active")
+    aStoryPage.setAttribute( "data-toggle","dropdown")
+    aStoryPage.setAttribute("role","button")
+    aStoryPage.setAttribute("aria-haspopup","true")
+    aStoryPage.setAttribute("aria-expanded", "false")
+    aStoryPage.innerText = "故事"
+
+    var spanStoryPage = document.createElement("span");
+    spanStoryPage.setAttribute("class", "caret");
+
+    aStoryPage.appendChild(spanStoryPage);
+
+    var ulStoryPage = document.createElement("ul");
+    ulStoryPage.setAttribute("class", "dropdown-menu");
+
+    var liStoryTrip = document.createElement("li");
+    var aStoryTrip = document.createElement("a");
+    aStoryTrip.setAttribute("href", mainPath + "story/旅途/story_trip_0.html");
+    aStoryTrip.innerText = "旅途";
+    liStoryTrip.appendChild(aStoryTrip);
+
+    var liStorySE = document.createElement("li");
+    var aStorySE = document.createElement("a");
+    aStorySE.setAttribute("href", mainPath + "notes/FCC_notes/note_0.html");
+    aStorySE.innerText = "支教";
+    liStorySE.setAttribute("class","disabled");
+    liStorySE.appendChild(aStorySE);
+
+    var liStoryElse = document.createElement("li");
+    var aStoryElse = document.createElement("a");
+    aStoryElse.setAttribute("href", mainPath + "notes/Interviews/interview_0.html");
+    aStoryElse.innerText = "杂七杂八";
+    liStoryElse.setAttribute("class","disabled");
+    liStoryElse.appendChild(aStoryElse);
+
+    ulStoryPage.appendChild(liStoryTrip);
+    ulStoryPage.appendChild(liStorySE);
+    ulStoryPage.appendChild(liStoryElse);
+    
+    liStoryPage.appendChild(aStoryPage);
+    liStoryPage.appendChild(ulStoryPage);
+
+    mainList.appendChild(liStoryPage);
+    // ---故事创建完毕
+});
